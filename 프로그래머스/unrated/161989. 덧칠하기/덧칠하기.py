dc4e456d65aa25a7dcaf165693c2502b6f30b42d
Yera Lee <57178359+Yera10@users.noreply.github.com>
2023-03-17 16:42:25 +0900

    [unrated] Title: 덧칠하기, Time: 3.03 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,9 +1,12 @@
+from collections import deque
+
 def solution(n, m, section):
+    section = deque(section)
     res = 1
-    t = section.pop(0) + m
+    t = section.popleft() + m
     
     while section:
-        s = section.pop(0)
+        s = section.popleft()
         if s >= t:
             res += 1
             t = s + m
