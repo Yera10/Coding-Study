b904c8999d680a85821b4d0aef25378d701c1f14
Yera Lee <57178359+Yera10@users.noreply.github.com>
2023-06-16 14:11:17 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 49804 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,26 +1,8 @@
 # SOLUTION
-def binary_search(arr, t, s, e):
-    if s > e:
-        return -1
-
-    m = (s+e)//2
-
-    if arr[m] < t:
-        return binary_search(arr, t, m+1, e)
-    elif arr[m] > t:
-        return binary_search(arr, t, s, m-1)
-    else:
-        return m
-
-
 def solution(arr, targets, N):
-    arr.sort()
-
+    arr = set(arr)
     for t in targets:
-        if binary_search(arr, t, 0, N-1)!=-1:
-            print(1)
-        else:
-            print(0)
+        print(1 if t in arr else 0)
 
 
 
