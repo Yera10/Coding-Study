09aa84f2f7333be5ac2391a62f36770752b7debe
Yera Lee <57178359+Yera10@users.noreply.github.com>
2023-06-29 11:37:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+# INPUT
+N = int(input())
+
+# SOL
+def solution(n):    
+    d = [1, 3] + [0] * (n - 2)
+
+    for i in range(2, n):
+        d[i] = (d[i-1] + d[i-2] * 2) % 10007
+    
+    return d[n - 1]
+    
+# OUTPUT
+print(solution(N))
